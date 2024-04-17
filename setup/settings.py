5036659fb86ce9b8bd912fa4f411cbbfb8df1941
Roan Deivd <roandeived51@gmail.com>
2024-04-17 17:23:00 -0300

    Estilização do projeto

--- setup/settings.py
+++ setup/settings.py
@@ -125,7 +125,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'setup/static')
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'satic')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
